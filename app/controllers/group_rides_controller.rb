5e0e6f1628281ef2682fb4555e229e72ec0745e4
Sarah Ketabchi <sarahketabchi@gmail.com>
2013-02-07 17:30:51 -0800

    Added organized rides to a users page

--- app/controllers/group_rides_controller.rb
+++ app/controllers/group_rides_controller.rb
@@ -3,11 +3,21 @@ class GroupRidesController < ApplicationController
     @group_ride = GroupRide.new
   end
 
-  # def show
-  # end
-
-
   def index
     @group_rides = GroupRide.all
+  end
+
+  def create
+    @group_ride = GroupRide.new(params[:group_ride])
+
+    if @group_ride.save
+      redirect_to group_ride_path(@group_ride)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @group_ride = GroupRide.find(params[:id])
   end
 end
--- app/models/attending_ride.rb
+++ app/models/attending_ride.rb
@@ -1,7 +1,11 @@
 class AttendingRide < ActiveRecord::Base
   # attr_accessible :title, :body
 
+  #validates :user_id, :group_ride_id, :presence => true
+
   belongs_to :group_ride
   #belongs_to :user
   belongs_to :rider, :class_name => "User"
+
+
 end
